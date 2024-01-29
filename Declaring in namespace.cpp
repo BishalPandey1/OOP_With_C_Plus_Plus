2784ea3e623e3a1bc7f6e4810b82f5732daa8c38
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2024-01-29 19:36:50 +0545

    Add files via upload

---
+++ Declaring in namespace.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float marks = 45;
+    std::cout<<"Marks in main method =" <<marks << std:: endl;
+     // std::cout<<"Marks in exam namespace =" <<exam::marks;
+    return 0;
+}
